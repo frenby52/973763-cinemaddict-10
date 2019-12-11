4828b1eacf29f5051eee9a1bf2c419523e523077
frenby52 <aakytin@mail.ru>
2019-12-11 13:05:19 +0300

    refactor sort

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,23 +1,17 @@
 import AbstractComponent from "./abstract-component";
 
-const sortTypes = {
-  default: `default`,
-  date: `date`,
-  rating: `rating`
-};
-
 const createSortTemplate = () =>
   `<ul class="sort">
-    <li><a href="#" data-sort-type="${sortTypes.default}" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" data-sort-type="${sortTypes.date}" class="sort__button">Sort by date</a></li>
-    <li><a href="#" data-sort-type="${sortTypes.rating}" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" data-sort-type="default" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" data-sort-type="date" class="sort__button">Sort by date</a></li>
+    <li><a href="#" data-sort-type="rating" class="sort__button">Sort by rating</a></li>
   </ul>`;
 
 export default class Sort extends AbstractComponent {
   constructor() {
     super();
 
-    this._sortType = sortTypes.default;
+    this._sortType = null;
   }
 
   getTemplate() {
@@ -29,8 +23,14 @@ export default class Sort extends AbstractComponent {
       evt.preventDefault();
       if (evt.target.classList.contains(`sort__button`) && evt.target.getAttribute(`data-sort-type`) !== this._sortType) {
         const sortElements = Array.from(this.getElement().querySelectorAll(`.sort__button`));
-        sortElements.forEach((it) => it.classList.remove(`sort__button--active`));
-        this._sortType = sortTypes[evt.target.getAttribute(`data-sort-type`)];
+        const defaultSortElementIndex = sortElements.findIndex((it) => it.getAttribute(`data-sort-type`) === `default`);
+        const activeSortElementIndex = sortElements.findIndex((it) => it.getAttribute(`data-sort-type`) === this._sortType);
+        if (this._sortType) {
+          sortElements[activeSortElementIndex].classList.remove(`sort__button--active`);
+        } else {
+          sortElements[defaultSortElementIndex].classList.remove(`sort__button--active`);
+        }
+        this._sortType = evt.target.getAttribute(`data-sort-type`);
         evt.target.classList.add(`sort__button--active`);
         handler(this._sortType);
       }
@@ -38,7 +38,7 @@ export default class Sort extends AbstractComponent {
   }
 
   getSortedData(data) {
-    if (this._sortType !== sortTypes.default) {
+    if (this._sortType !== `default`) {
       return data.slice().sort((a, b) => b[this._sortType] - a[this._sortType]);
     }
     return data;
